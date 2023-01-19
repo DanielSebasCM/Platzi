bf5fca0bfbadc88ab03be3a7e371cfcd854836da
DanielSebasCM <danielsebascm@hotmail.com>
2023-01-18 19:16:37 -0500

    Finished Game

--- JS_BASICO/Videojuegos/Snake/main.js
+++ JS_BASICO/Videojuegos/Snake/main.js
@@ -1,18 +1,19 @@
 'use strict';
 window.addEventListener('load', function () {
   /** @type {HTMLCanvasElement} */
-  let canvas = document.getElementById('game-canvas');
-  let game = canvas.getContext('2d');
+  const canvas = document.getElementById('game-canvas');
+  const game = canvas.getContext('2d');
+  const scoreSpan = document.getElementById('score');
 
-  let aspectRatio = 16 / 9;
-  let gridSize = 2 * 16;
+  const aspectRatio = 16 / 9;
+  const gridSize = 2 * 16;
 
   let canvasSize;
   let elementSize;
-  let speed = 2;
+  let speed = 5;
 
-  let snake = {
-    length: 8,
+  const snake = {
+    length: 3,
     x: 0,
     y: 0,
     direction: [1, 0],
@@ -20,34 +21,12 @@ window.addEventListener('load', function () {
   };
   snake.body.unshift([snake.x, snake.y]);
 
+  scoreSpan.innerText = (snake.length - 3) * 100;
   let food = getFood();
 
   resizeCanvas();
 
-  let gameUpdateInterval = setInterval(() => {
-    snake.x += snake.direction[0];
-    snake.y += snake.direction[1];
-    snake.body.unshift([snake.x, snake.y]);
-
-    if (snake.body.length > snake.length) {
-      snake.body.pop();
-    }
-
-    let isSnakeOutOfBounds =
-      snake.x < 0 || snake.x >= gridSize ||
-      snake.y < 0 || snake.y >= gridSize / aspectRatio;
-
-    let didSnakeEatItself = snake.body.some((pos, index) => {
-      return index !== 0 && pos[0] === snake.x && pos[1] === snake.y;
-    });
-    if (isSnakeOutOfBounds || didSnakeEatItself) {
-      gameOver();
-      return;
-    }
-    renderGame();
-  }, 1000 / speed);
-
-
+  let gameUpdateInterval = setInterval(updateGameState, 1000 / speed);
 
   window.addEventListener('keydown', (event) => {
     switch (event.key) {
@@ -68,6 +47,37 @@ window.addEventListener('load', function () {
 
   window.addEventListener('resize', resizeCanvas);
 
+  function updateGameState() {
+    snake.x += snake.direction[0];
+    snake.y += snake.direction[1];
+    snake.body.unshift([snake.x, snake.y]);
+
+    if (snake.body.length > snake.length) {
+      snake.body.pop();
+    }
+
+    let isSnakeOutOfBounds =
+      snake.x < 0 || snake.x >= gridSize ||
+      snake.y < 0 || snake.y >= gridSize / aspectRatio;
+
+    let didSnakeEatItself = snake.body.some((pos, index) => {
+      return index !== 0 && pos[0] === snake.x && pos[1] === snake.y;
+    });
+    if (isSnakeOutOfBounds || didSnakeEatItself) {
+      gameOver();
+      return;
+    }
+
+    if (snake.x === food.x && snake.y === food.y) {
+      snake.length++;
+      food = getFood();
+      speed += 0.5;
+      scoreSpan.innerText = (snake.length - 3) * 100;
+    }
+
+    renderGame();
+  }
+
   function resizeCanvas() {
     canvasSize = Math.min(window.innerHeight * aspectRatio, window.innerWidth) * 0.6;
     canvas.width = canvasSize;
@@ -79,11 +89,18 @@ window.addEventListener('load', function () {
   function renderGame() {
     game.fillStyle = "#788374";
     game.clearRect(0, 0, canvas.width, canvas.height);
+    let snakePadding = elementSize * 0.01;
+    let snakeSize = elementSize - snakePadding * 2;
     snake.body.forEach((element) => {
-      game.fillRect(element[0] * elementSize, element[1] * elementSize, elementSize, elementSize);
+      game.fillRect(
+        element[0] * elementSize + snakePadding,
+        element[1] * elementSize + snakePadding,
+        snakeSize, snakeSize);
     });
     game.fillStyle = "#aa644d";
-    game.fillRect(food.x * elementSize, food.y * elementSize, elementSize, elementSize);
+    let foodPadding = elementSize * 0.15;
+    let foodSize = elementSize - foodPadding * 2;
+    game.fillRect(food.x * elementSize + foodPadding, food.y * elementSize + foodPadding, foodSize, foodSize);
   }
 
   function gameOver() {
